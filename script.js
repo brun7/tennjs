b6d25322d3b41a7412f0b20ef0e01f7701d1ba89
Brian J. Brun <brian.brun7@gmail.com>
2025-02-14 09:25:19 -0600

    Collison Detection of top and bottom of paddles

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let width = canvas.width;
 let height = canvas.height;
 
 const BALL_SIZE = 5;
-let ballPosition = {x:20, y:30};
+let ballPosition = { x: 20, y: 30 };
 
 let xSpeed = 4;
 let ySpeed = 2;
@@ -17,7 +17,7 @@ const PADDLE_OFFSET = 10;
 let leftPaddleTop = 10;
 let rightPaddleTop = 30;
 
-document.addEventListener("mousemove", e => {
+document.addEventListener("mousemove", (e) => {
   rightPaddleTop = e.y - canvas.offsetTop;
 });
 
@@ -25,7 +25,6 @@ function draw() {
   drawBackground();
   drawBall();
   drawPaddles();
-
 }
 
 function update() {
@@ -44,12 +43,25 @@ function drawBall() {
 }
 
 function drawPaddles() {
-  ctx.fillRect( PADDLE_OFFSET, leftPaddleTop, PADDLE_WIDTH, PADDLE_HEIGHT);
-  ctx.fillRect( width - PADDLE_WIDTH - PADDLE_OFFSET, rightPaddleTop, PADDLE_WIDTH, PADDLE_HEIGHT);
+  ctx.fillRect(PADDLE_OFFSET, leftPaddleTop, PADDLE_WIDTH, PADDLE_HEIGHT);
+  ctx.fillRect(
+    width - PADDLE_WIDTH - PADDLE_OFFSET,
+    rightPaddleTop,
+    PADDLE_WIDTH,
+    PADDLE_HEIGHT
+  );
+}
+
+function adjustAngle(distanceFromTop, distanceFromBottom) {
+  if (distanceFromTop < 0) {
+    ySpeed -= 0.5;
+  } else if (distanceFromBottom < 0) {
+    ySpeed += 0.5;
+  }
 }
 
 function checkPaddleCollision(ball, paddle) {
-  return(
+  return (
     ball.left < paddle.right &&
     ball.right > paddle.left &&
     ball.top < paddle.bottom &&
@@ -62,27 +74,33 @@ function checkCollision() {
     left: ballPosition.x,
     right: ballPosition.x + BALL_SIZE,
     top: ballPosition.y,
-    bottom: ballPosition.y + BALL_SIZE
-  }
+    bottom: ballPosition.y + BALL_SIZE,
+  };
 
   let leftPaddle = {
     left: PADDLE_OFFSET,
     right: PADDLE_OFFSET + PADDLE_WIDTH,
     top: leftPaddleTop,
-    bottom: leftPaddleTop + PADDLE_HEIGHT
-  }
+    bottom: leftPaddleTop + PADDLE_HEIGHT,
+  };
 
   let rightPaddle = {
     left: width - PADDLE_WIDTH - PADDLE_OFFSET,
     right: width - PADDLE_OFFSET,
     top: rightPaddleTop,
-    bottom: rightPaddleTop + PADDLE_HEIGHT
-  }
+    bottom: rightPaddleTop + PADDLE_HEIGHT,
+  };
 
-  if( checkPaddleCollision(ball, leftPaddle) ) {
+  if (checkPaddleCollision(ball, leftPaddle)) {
+    let distanceFromTop = ball.top - leftPaddle.top;
+    let distanceFromBottom = leftPaddle.bottom - ball.bottom;
+    adjustAngle(distanceFromTop, distanceFromBottom);
     xSpeed = Math.abs(xSpeed);
   }
-  if( checkPaddleCollision(ball, rightPaddle) ) {
+  if (checkPaddleCollision(ball, rightPaddle)) {
+    let distanceFromTop = ball.top - rightPaddle.top;
+    let distanceFromBottom = rightPaddle.bottom - ball.bottom;
+    adjustAngle(distanceFromTop, distanceFromBottom);
     xSpeed = -Math.abs(xSpeed);
   }
 
@@ -104,4 +122,3 @@ function gameLoop() {
 }
 
 gameLoop();
-
