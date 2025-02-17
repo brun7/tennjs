2ab1e9b427c9a0d2582ec226c8f84977c931fa07
Brian J. Brun <brian.brun7@gmail.com>
2025-02-17 15:51:58 -0600

    Add the Paddle Class

--- script.js
+++ script.js
@@ -54,3 +54,12 @@ class Entity {
   }
 }
 
+class Paddle extends Entity {
+  static WIDTH = 5;
+  static HEIGHT = 20;
+  static OFFSET = 10;
+
+  constructor(x,y) {
+    super(x, y, Paddle.WIDTH, Paddle.HEIGHT);
+  }
+}
